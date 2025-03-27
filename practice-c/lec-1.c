e2d9f023af9c6aed15c2749e5d6012efd954df5d
aqsashah <aqsanor665@gmail.com>
2025-03-27 16:52:36 +0500

    variables, input done

---
+++ practice-c/lec-1.c
@@ -0,0 +1,124 @@
+# include<stdio.h>
+
+// int main() {
+//     printf("hello world");
+//     return 0;
+// }
+
+
+// VARIABLES
+// variables is the name of a memory location which stores some data.
+
+
+//int main() {
+    // int number = 22;
+    // char star = '*';
+    // int age = 20;
+
+    // variabl hamesha change hosakta
+    // age = 24;
+
+    // float pi = 2.14;
+
+    // case sensitive
+    // int a = 28;
+    // int A = 21;
+
+    // start with alphabet or underscore
+    // int _age = 22;
+
+    // or variables k nam m space hi nhi de sakte 
+//     int final_price = 100;
+//     return 0;
+// }
+
+// c m string or boolean nhi hota
+
+
+// DATA TYPES
+// int main() {
+//     int age = 22;
+//     float pi = 2.2;
+//     char hashtag = '#';
+//     return 0;
+// }
+
+
+// CONSTANTS
+// fixed
+
+
+// KEYWORDS
+// reserved words that have special meaning to the compiler.
+// 32 keywords in C
+
+
+// PROGRAM STRUCTURE
+// #include<stdio.h> YE ZARURI h is k bager code nhi chalega
+
+
+// OUTPUT
+// int main() {
+// printf("aqsa shah");
+// return 0;
+// }
+
+// \n means new line
+// int main() {
+//     printf("hy aqsa \n");
+//     printf(" hy aqsa \n");
+//     return 0;
+// }
+
+// output cases
+// 1 integers
+// printf("age is %d", age)
+// 2 real numbers
+// %f
+// 3 characters
+// %c
+// y sar formate spaifirs hn
+
+// int main() {
+//     int age = 20;
+//     printf("age is %d \n", age);
+//     return 0;
+// }
+
+// int main() {
+//     float pi = 3.14;
+//     printf("age is %f \n", age);
+//     return 0;
+// }
+
+
+// INPUT
+// scanf("%d", &age);
+
+// int main() {
+//     int age ;
+//     printf("enter age");
+//     scanf("%d", &age);
+//     printf("age is : %d", age);
+//     return 0;
+//}
+
+// int main() {
+//     int a, b;
+//     printf("enter a");
+//     scanf("%d", &a);
+
+//     printf("enter b");
+//     scanf("%d", &b);
+
+//     int sum  = a + b;
+//     printf("sum is : %d", sum);
+//     return 0;
+// }
+
+
+// COMPILATION
+// A computer program that translate C code into machine code
+
+QUESTIONS
+WRITE a program to calcul
