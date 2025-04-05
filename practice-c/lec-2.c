d37c067811d575cf8500cd0c2b95daa7dc91b228
aqsashah <aqsanor665@gmail.com>
2025-04-05 22:51:05 +0500

    .

---
+++ practice-c/lec-2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+/*instructions
+these are statements in a program
+types
+1. type declaration 
+2. arithmetic
+3. control
+
+1.type declaration instructions => declare variable before using it */
+// int main() {
+//     int a = 10;
+//     int b = a;
+//     int c = b * 6;
+//     int d = 1, e;
+
+//     int oldAge = 22;
+//     int years = 2;
+//     int newage = oldAge + years;
+
+//     return 0;
+// }
+
+// arithmetic instructions
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int b, c;
+    b = c = 1;
+    int a = b + c;
+    int power = (int)pow(b, c);  // Casting pow result to int
+    printf("a = %d, power = %d\n", a, power);
+    return 0;
+}
