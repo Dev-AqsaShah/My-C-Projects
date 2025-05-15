d539498cbe465ecae6ef262e99eac780892dd8a7
aqsashah <aqsanor665@gmail.com>
2025-05-15 21:14:05 +0500

    Create 05-function&recursion.c

---
+++ practice-c/05-function&recursion.c
@@ -0,0 +1,32 @@
+//  FUNCTIONS
+
+// block of code that performs particular task
+
+// syntax 1
+// function prototype --> void printHello();
+
+// syntax 2 
+// function  definition
+// void printHello() {
+//     printf("Hello");
+// }
+
+// syntax 3 
+// function call
+//  int main() {
+//     printHello();
+//     return 0
+// }
+
+
+#include<stdio.h>
+
+void printHello();
+
+int main() {
+
+    return 0;
+}
+void printHello() {
+    printf("Hello!");
+}
